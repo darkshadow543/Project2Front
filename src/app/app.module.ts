fa6f947f67fceb6be7e73127fdd1d8d3c6e0a3d9
Ronald Lee <ronaldlee8727@gmail.com>
2019-07-18 19:42:25 -0400

    create layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { BodyComponent } from './body/body.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { MainComponent } from './layouts/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
-    BodyComponent
+    BodyComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
--- src/app/layouts/main/main.component.spec.ts
+++ src/app/layouts/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeadComponent } from './head.component';
+import { MainComponent } from './main.component';
 
-describe('HeadComponent', () => {
-  let component: HeadComponent;
-  let fixture: ComponentFixture<HeadComponent>;
+describe('MainComponent', () => {
+  let component: MainComponent;
+  let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeadComponent ]
+      declarations: [ MainComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HeadComponent);
+    fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });

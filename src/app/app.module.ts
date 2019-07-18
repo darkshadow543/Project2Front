95c87b8b8db594f8e4328d2a6e9fad3d8031dcd0
unknown <sbengel@hotmail.com>
2019-07-18 08:54:40 -0400

    Added Head-Body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,

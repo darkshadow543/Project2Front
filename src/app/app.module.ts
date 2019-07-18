68e58a414c249c8c9b6db2d4f1f2e63629ec0a3b
unknown <sbengel@hotmail.com>
2019-07-18 15:52:58 -0400

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadComponent } from './head/head.component';
 import { BodyComponent } from './body/body.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ChanOwnComponent } from './components/chan-own/chan-own.component';
+import { ChanFormComponent } from './components/chan-form/chan-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeadComponent,
-    BodyComponent
+    BodyComponent,
+    NavComponent,
+    ChanOwnComponent,
+    ChanFormComponent
   ],
   imports: [
     BrowserModule,

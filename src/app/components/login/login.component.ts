a5d952148f084e256af7c7abcfe885e67ae80341
Ronald Lee <ronaldlee8727@gmail.com>
2019-07-18 20:10:57 -0400

    modified login, register, header, app-routing modules

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email:string;
+  password:string;
+
+  constructor(private userService:UserService, private router:Router) { }
 
   ngOnInit() {
   }
 
+  login(){
+     console.log("Login:", this.email, this.password);
+     this.userService.login(this.email, this.password).subscribe ( (res)=>{
+       console.log(res);
+       this.router.navigate(['listusers']);
+     });
+ 
+  }
 }

1fcb7de6c45a6996a0613440752fd4b60f002e70
darkshadow543 <sbengel@hotmail.com>
2019-07-22 22:37:52 -0400

    Fixed Merge

--- src/app/components/view-all-channels/view-all-channels.component.ts
+++ src/app/components/view-all-channels/view-all-channels.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ChannelService } from 'src/app/services/channel.service';
 import { Channel } from 'src/app/models/channel';
-import { userInfo } from 'os';
-import { User } from 'src/app/models/user';
-import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-view-all-channels',
@@ -14,7 +11,7 @@ import { UserService } from 'src/app/services/user.service';
 export class ViewAllChannelsComponent implements OnInit {
 
   viewedChannel:Channel;
-  constructor(private channelService: ChannelService, private userService, private router: Router) { }
+  constructor(private channelService: ChannelService) { }
 
   ngOnInit() {
     this.list();
@@ -29,11 +26,4 @@ export class ViewAllChannelsComponent implements OnInit {
     });
   }
 
-<<<<<<< HEAD
-=======
-  subscribe(id:number){
-    
-  }
-
->>>>>>> 80b2ee37776f50605cfdff43f6ebfecb925c5b32
 }
